301163583ad2e24e4dc75c1401fb80f11b16e2dc
Calvin Cheng <linchuan.cheng@gmail.com>
2015-05-15 00:14:06 +0800

    Clean up

--- TypeClasses/subclasses.hs
+++ TypeClasses/subclasses.hs
@@ -1,6 +1,8 @@
 -- Ord is a subclass of Eq
 --
 
+module Main where
+
 class (Eq a) => Ord a where
     (<) :: a -> a -> Bool
     (>) :: a -> a -> Bool
@@ -11,3 +13,6 @@ class (Eq a) => Ord a where
     min :: a -> a -> a
 
 data Ordering' = LT | EQ | GT
+
+main ::  IO ()
+main = print "hello"
